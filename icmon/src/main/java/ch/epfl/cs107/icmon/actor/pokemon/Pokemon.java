f4ee8a756ac9517516ac093e9a9482cdcd9aae26
Jakub Kliment <jakub.kliment@epfl.ch>
2023-12-16 16:52:26 +0100

    Pokemon

--- icmon/src/main/java/ch/epfl/cs107/icmon/actor/pokemon/Pokemon.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/actor/pokemon/Pokemon.java
@@ -1,11 +1,38 @@
 package ch.epfl.cs107.icmon.actor.pokemon;
 
+import ch.epfl.cs107.icmon.actor.ICMonActor;
+import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.engine.actor.RPGSprite;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Orientation;
+import ch.epfl.cs107.play.window.Canvas;
+
 /**
  * ???
  *
  * @author Hamza REMMAL (hamza.remmal@epfl.ch)
  */
-public abstract class Pokemon {
+public abstract class Pokemon extends ICMonActor {
+
+    private String name;
+    private int hp;
+    private final int hpMax;
+    private final int attackDamage;
+    /**
+     * Default MovableAreaEntity constructor
+     *
+     * @param area        (Area): Owner area. Not null
+     * @param orientation (Orientation): Initial orientation of the entity. Not null
+     * @param position    (Coordinate): Initial position of the entity. Not null
+     */
+    public Pokemon(Area area, Orientation orientation, DiscreteCoordinates position, String name, int attackDamage, int hpMax) {
+        super(area, orientation, position);
+        this.name = name;
+        this.attackDamage = attackDamage;
+        this.hpMax = hpMax;
+        hp = hpMax;
+    }
 
     /**
      * @author Hamza REMMAL (hamza.remmal@epfl.ch)
@@ -30,4 +57,33 @@ public abstract class Pokemon {
 
     }
 
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+    @Override
+    public boolean takeCellSpace() {
+        return false;
+    }
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {
+        if (isCellInteraction) {
+            v.interactWith(this, true);
+        }
+    }
+    @Override
+    public void draw(Canvas canvas) {
+        new RPGSprite("pokemon/" + name, 1, 1, this);
+    }
+    public boolean isAlive() {
+        return hp > 0;
+    }
+    public void receiveAttack(int damage) {
+        if (isAlive()) {
+            hp -= damage;
+        }
+        if (hp < 0) {
+            hp = 0;
+        }
+    }
 }
---
+++ icmon/src/main/java/ch/epfl/cs107/icmon/area/maps/Arena.java
@@ -0,0 +1,30 @@
+package ch.epfl.cs107.icmon.area.maps;
+
+import ch.epfl.cs107.icmon.actor.Door;
+import ch.epfl.cs107.icmon.actor.pokemon.Bulbizarre;
+import ch.epfl.cs107.icmon.area.ICMonArea;
+import ch.epfl.cs107.play.engine.actor.Background;
+import ch.epfl.cs107.play.engine.actor.Foreground;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.math.Orientation;
+
+public class Arena extends ICMonArea {
+
+    @Override
+    public String getTitle() {
+        return "arena";
+    }
+
+    @Override
+    protected void createArea() {
+        registerActor(new Background(this));
+        registerActor(new Foreground((this)));
+        registerActor(new Bulbizarre(this, Orientation.DOWN, new DiscreteCoordinates(6, 6)));
+        registerActor(new Door(this, new DiscreteCoordinates(20,15), "town", new DiscreteCoordinates(15, 23), new DiscreteCoordinates(4, 1), new DiscreteCoordinates(5, 1)));
+    }
+
+    @Override
+    public DiscreteCoordinates getPlayerSpawnPosition() {
+        return null;
+    }
+}
